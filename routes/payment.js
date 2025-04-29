d6ec12cd2bb2e9fd4125c0b5df12c5ce60ca90e1
Evangel Puri <purificationevan04@gmail.com>
2025-04-29 08:33:22 +0600

    x

--- routes/payment.js
+++ routes/payment.js
@@ -26,9 +26,8 @@ router.post("/", async (req, res) => {
                 },
             ],
             mode: "payment",
-            // success_url: "https://funding-front-end-three.vercel.app/payment/success",
-            success_url: "http://localhost:3000/payment/success",
-            cancel_url: "http://localhost:3000/payment/cancel",
+            success_url: "https://funding-front-end-three.vercel.app/payment/success",
+            cancel_url: "https://funding-front-end-three.vercel.app/payment/cancel",
             metadata: {
                 name,
                 phone,
